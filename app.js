879ed0819f04bd1174a306b703b63d539c6a9eaf
Avishka Indula <avishkainduladev@gmail.com>
2022-11-17 16:58:38 +0530

    moving session configuration settings to session.js inside config file

--- app.js
+++ app.js
@@ -2,22 +2,33 @@ const path = require("path");
 
 const express = require("express");
 const session = require("express-session");
-const mongodbStore = require("connect-mongodb-session");
 const csrf = require("csurf");
+
+// const mongodbStore = require("connect-mongodb-session");
+// We've moved this code to the config.js file.
+
+const sessionConfig = require("./config/session");
+// We've moved the session configuration logic into another file called config.js
+// So in order to use that logic, we need to require that like this.
 
 const db = require("./data/database");
 const authRoutes = require("./routes/auth");
 const blogRoutes = require("./routes/blog");
 
-const MongoDBStore = mongodbStore(session);
+const mongoDbSessionStore = sessionConfig.createSessionStore(session);
+// This is how we execute the createSessionStore function inside the config.js file.
+
+// const MondoDBStore = mongodbStore(session);
+// We've moved this code to the config.js file.
 
 const app = express();
 
-const sessionStore = new MongoDBStore({
-  uri: "mongodb+srv://avishka_indula:p7iGGaREtxbhN3t3@cluster0.ibnu8y4.mongodb.net/test",
-  databaseName: "auth-blog",
-  collection: "sessions",
-});
+// const sessionStore = new MongoDBStore({
+//   uri: "mongodb+srv://avishka_indula:p7iGGaREtxbhN3t3@cluster0.ibnu8y4.mongodb.net/test",
+//   databaseName: "auth-blog",
+//   collection: "sessions",
+// });
+// We've moved this code to the config.js file.
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -25,17 +36,22 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
-app.use(
-  session({
-    secret: "super-secret",
-    resave: false,
-    saveUninitialized: false,
-    store: sessionStore,
-    cookie: {
-      maxAge: 2 * 24 * 60 * 60 * 1000,
-    },
-  })
-);
+// app.use(
+//   session({
+//     secret: "super-secret",
+//     resave: false,
+//     saveUninitialized: false,
+//     store: sessionStore,
+//     cookie: {
+//       maxAge: 2 * 24 * 60 * 60 * 1000,
+//     },
+//   })
+// );
+// We can move the object inside app.use into the config.js file
+// So now we can execute the following line and execute createSessionConfig function
+// inside the session.js file in order to do what's we've done above.
+app.use(session(sessionConfig.createSessionConfig(mongoDbSessionStore)));
+
 app.use(csrf());
 
 app.use(async function (req, res, next) {
---
+++ config/session.js
@@ -0,0 +1,30 @@
+const mongodbStore = require("connect-mongodb-session");
+
+function createSessionStore(session) {
+  const MongoDBStore = mongodbStore(session);
+
+  const sessionStore = new MongoDBStore({
+    uri: "mongodb+srv://avishka_indula:p7iGGaREtxbhN3t3@cluster0.ibnu8y4.mongodb.net/test",
+    databaseName: "auth-blog",
+    collection: "sessions",
+  });
+
+  return sessionStore;
+}
+
+function createSessionConfig(sessionStore) {
+  return {
+    secret: "super-secret",
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+    cookie: {
+      maxAge: 2 * 24 * 60 * 60 * 1000,
+    },
+  };
+}
+
+module.exports = {
+  createSessionStore: createSessionStore,
+  createSessionConfig: createSessionConfig,
+};
